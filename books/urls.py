7ba40c5faa4b169c56fc13106e9107578d61eefc
Gatsby Herr <gatsbyherr26@gmail.com>
2021-06-22 16:38:41 -0700

    first commit

---
+++ books/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.urls.conf import include
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('books', views.index),
+    path('books/create', views.create_book),
+    path('books/<int:book_id>', views.show_book),
+    path('books/<int:book_id>/assign', views.assign_book),
+    path('authors/<int:author_id>/assign', views.assign_author),
+    path('authors', views.authors),
+    path('authors/create', views.create_author),
+    path('authors/<int:author_id>', views.show_author),
+    path('authors/create', views.create_author),
